90ab2c71aacd99c4a8e9bbfb5d0e0c190aed0636
Guney Dirim <passarante@gmail.com>
2020-04-13 20:52:54 +0300

    Basic scaffolding and basic auth scaffolding

--- server.js
+++ server.js
@@ -1,17 +1,88 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const colors = require('colors');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const fileupload = require('express-fileupload');
 const connectDB = require('./config/db');
+const errorHandler = require('./middlewares/errorHandler');
 
-// Load app variables
+// Load env vars
 dotenv.config({ path: './config/config.env' });
 
-// Connect Database
+// Connect to Database
 connectDB();
+
+// Route files
+const auth = require('./routes/auth');
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Dev logging middleware
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'));
+}
+
+// File uploading
+app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate Limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Mount routes
+app.use('/api/v1/auth', auth);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server runnign on port ${PORT}`.yellow.bold);
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
+
+// Handle unhandled promise rejections
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message} `);
+  // Close server & exit process
+  server.close(() => process.exit(1));
 });
