13889a7965939cec1515a9bc2146c11309ca601f
Guney Dirim <passarante@gmail.com>
2020-04-13 17:32:15 +0300

    Added mongoose and connected database

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load app variables
 dotenv.config({ path: './config/config.env' });
+
+// Connect Database
+connectDB();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server runnign on port ${PORT}`);
+  console.log(`Server runnign on port ${PORT}`.yellow.bold);
 });
